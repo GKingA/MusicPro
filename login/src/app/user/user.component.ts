39e5e1bfd33da00f40cb83d724fcd6fd4b45eef7
lavatilivi <lavatil@gmail.com>
2018-11-20 21:40:39 +0100

    user imports

--- login/src/app/user/user.component.ts
+++ login/src/app/user/user.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Http, HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import {AppComponent} from '../app.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [ BrowserModule, HttpModule ],
+    providers: [],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+})
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
+
 export class UserComponent implements OnInit {
 
   constructor() { }
